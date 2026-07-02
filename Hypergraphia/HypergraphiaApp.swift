fb4d6bd9402c40b3886be846f6f0c823dd9f2c9c
Sameer Ismail <sam@samism.io>
2026-07-02 15:12:29 -0500

    [mac] Hide native tab bar add button

--- Hypergraphia/HypergraphiaApp.swift
+++ Hypergraphia/HypergraphiaApp.swift
@@ -39,6 +39,8 @@ final class HypergraphiaAppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationWillFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
+
         // Avoid Dock-icon flash when the user launches with the toggle on.
         // The `didBecomeMain` observer flips us back to `.regular` once a
         // document window appears (Finder-open, untitled-on-launch, etc.).
